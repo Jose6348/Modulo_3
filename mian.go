668073b550bf5d9500242b16fdacf15165e29f63
Jose6348 <jfalasco88@gmail.com>
2025-02-18 23:39:59 -0300

    Http Servers

--- mian.go
+++ mian.go
@@ -1,15 +1,32 @@
 package main
 
-type Foo struct {
-	Bar string
-	Baz float64
-	quz []string
-}
+import (
+	"errors"
+	"fmt"
+	"net/http"
+	"time"
+)
 
 func main() {
-	f := Foo{
-		Bar: "",
-		Baz: 0,
-		quz: []string{},
+	mux := http.NewServeMux()
+	mux.HandleFunc(
+		"/healthcheck",
+		func(w http.ResponseWriter, r *http.Request) {
+			fmt.Fprintln(w, "hello, world")
+		},
+	)
+	srv := &http.Server{
+		Addr:                         ":8080",
+		Handler:                      mux,
+		DisableGeneralOptionsHandler: false,
+		ReadTimeout:                  10 * time.Second,
+		WriteTimeout:                 10 * time.Second,
+		IdleTimeout:                  1 * time.Minute,
 	}
+	if err := srv.ListenAndServe(); err != nil {
+		if !errors.Is(err, http.ErrServerClosed) {
+			panic(err)
+		}
+	}
+	fmt.Println(1)
 }
