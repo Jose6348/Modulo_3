4ef774cfbe12ea32088a21895798ab960004a3e5
Jose6348 <jfalasco88@gmail.com>
2025-02-18 23:27:17 -0300

    Fill Struct

--- mian.go
+++ mian.go
@@ -1,35 +1,15 @@
 package main
 
-import (
-	"context"
-	"fmt"
-	"io"
-	"net/http"
-	"time"
-)
+type Foo struct {
+	Bar string
+	Baz float64
+	quz []string
+}
 
 func main() {
-	ctx := context.Background()
-	ctx, cancel := context.WithTimeout(ctx, 5*time.Second)
-	defer cancel()
-
-	req, err := http.NewRequestWithContext(ctx, http.MethodGet, "https://www.google.com", nil)
-	if err != nil {
-		panic(err)
+	f := Foo{
+		Bar: "",
+		Baz: 0,
+		quz: []string{},
 	}
-
-	req.Header.Set("ACCEPT", "application/json")
-
-	resp, err := http.DefaultClient.Do(req)
-	if err != nil {
-		panic(err)
-	}
-	defer resp.Body.Close()
-
-	data, err := io.ReadAll(resp.Body)
-	if err != nil {
-		panic(err)
-	}
-
-	fmt.Println(string(data))
 }
